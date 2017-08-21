3b5b1fc6995c035013fd4371b53e4ed056e7ab96
Jitender S. Padda <jitenderpadda@gmail.com>
2017-08-21 22:46:14 +1000

    BST

--- src/BstMultiset.java
+++ src/BstMultiset.java
@@ -1,23 +1,68 @@
 import java.io.PrintStream;
 import java.util.*;
 
-import LinkedListMultiset.Node;
-
-public class BstMultiset<T> extends Multiset<T>
+public class BstMultiset<T extends Comparable<T>> extends Multiset<T> implements Comparator<T>
 {
+	Node root;
 	public BstMultiset() {
-		// Implement me!
+		root=new Node(null, null, null, 0);
 	} // end of BstMultiset()
 
 	public void add(T item) {
-		// Implement me!
+		Node currentNode=root;
+		//Condition for Empty Tree
+		if(currentNode==root && currentNode.getElement()==null){
+			currentNode.setElement(item);
+			currentNode.setFreq(1);
+			System.out.println("rootAdd--");
+			return;
+		}
+		addTraverse(currentNode, item);
+		
 	} // end of add()
-
-
+	public void addTraverse(Node currentNode, T item){
+		System.out.println("CurrentNode.getElement()---"+currentNode.getElement());
+		System.out.println("item---"+item);
+		//if item is smaller than current Element
+		if(currentNode.getElement().compareTo(item)==1){
+			//if Node has left child - Recursive call
+			if(currentNode.hasLeft()){
+				addTraverse(currentNode.getLeft(), item);
+			}// else Add new left child Node
+			else {
+				System.out.println("leftAdd--");
+				Node newNode=new Node(null, null, item, 1);
+				currentNode.setLeft(newNode); 
+			}
+		}//else if Node has right child - Recursive call
+		else if(currentNode.getElement().compareTo(item)==-1){
+			if(currentNode.hasRight()){
+				addTraverse(currentNode.getRight(), item);
+			}//else Add new right child Node
+			else {
+				System.out.println("rightAdd--");
+				Node newNode=new Node(null, null, item, 1);
+				currentNode.setRight(newNode); 
+			}
+		}//else if item = element then freq + 1
+		else if(currentNode.getElement().compareTo(item)==0){
+			currentNode.setFreq(currentNode.getFreq()+1);
+			return;
+		}
+	}
 	public int search(T item) {
-		// Implement me!
-
-		// default return, please override when you implement this method
+		Node currentNode=root;
+		while(currentNode.hasLeft() || currentNode.hasRight()){
+			if(currentNode.getElement().compareTo(item)==1){
+				currentNode=currentNode.getLeft();
+			}
+			else if(currentNode.getElement().compareTo(item)==-1){
+				currentNode=currentNode.getRight();
+			}
+			else if(currentNode.getElement().compareTo(item)==0){
+				return currentNode.getFreq();
+			}
+		}
 		return 0;
 	} // end of add()
 
@@ -33,8 +78,19 @@ public class BstMultiset<T> extends Multiset<T>
 
 
 	public void print(PrintStream out) {
-		// Implement me!
+		Node currentNode=root;
+		printTraverse(currentNode, out);
 	} // end of print()
+	
+	public void printTraverse(Node currentNode, PrintStream out){
+		if(currentNode.hasLeft()){
+			printTraverse(currentNode.getLeft(), out);
+		}
+		out.println(currentNode.getElement() + printDelim + currentNode.getFreq());
+		if(currentNode.hasRight()){
+			printTraverse(currentNode.getRight(), out);
+		}
+	}
 	
 	//Node Class
 	public class Node{
@@ -43,20 +99,20 @@ public class BstMultiset<T> extends Multiset<T>
 		private T element;
 		private int freq;
 		
-		public Node(Node next, Node prev,T element, int freq){
-			this.next=next;
-			this.prev=prev;
+		public Node(Node left, Node right,T element, int freq){
+			this.left=left;
+			this.right=right;
 			this.element=element;
 			this.freq=freq;
 		}
 			
 		//set Frequency
-		public int setfreq(int freq){
+		public int setFreq(int freq){
 			return this.freq=freq;
 		}
 		
 		//get Frequency
-		public int getfreq(){
+		public int getFreq(){
 			return this.freq;
 		}
 				
@@ -64,35 +120,45 @@ public class BstMultiset<T> extends Multiset<T>
 		public T getElement(){
 			return this.element;
 		}
+		
+		//set Element
+		public void setElement(T element){
+			this.element=element;
+		}
 					
-		//get Next Node
-		public Node getNext(){
-			return this.next;
+		//get Left Node
+		public Node getLeft(){
+			return this.left;
 		}
 		
-		//get Previous Node
-		public Node getPrev(){
-			return this.prev;
+		//get Right Node
+		public Node getRight(){
+			return this.right;
 		}
 		
-		//set Next Node
-		public void setNext(Node next){
-			this.next=next;
+		//set Right Node
+		public void setRight(Node right){
+			this.right=right;
 		}
 				
-		//set Previous Node
-		public void setPrev(Node prev){
-			this.prev=prev;
+		//set Left Node
+		public void setLeft(Node left){
+			this.left=left;
 		}
 			
-		//check if has Next Node
-		public boolean hasNext(){
-			return this.getNext()!=tail?true:false;
+		//check if has Left Node
+		public boolean hasLeft(){
+			return this.getLeft()!=null?true:false;
 		}
 							
-		//check if has Previous Node
-		public boolean hasPrev(){
-			return this.getPrev()!=head?true:false;
+		//check if has Right Node
+		public boolean hasRight(){
+			return this.getRight()!=null?true:false;
 		}
+	}
+
+	@Override
+	public int compare(T o1, T o2) {
+		return o1.compareTo(o2);
 	}
 } // end of class BstMultiset
--- src/mainClass.java
+++ src/mainClass.java
@@ -2,32 +2,26 @@
 public class mainClass {
 
 	public static void main(String[] args) {
-		/*LinkedListMultiset<Integer> multiSet=new LinkedListMultiset<Integer>();
+		BstMultiset<Integer> multiSet=new BstMultiset<Integer>();
+		/*SortedLinkedListMultiset<Integer> multiSet=new SortedLinkedListMultiset<Integer>();*/
 		System.out.println("Print Empty MultiSet");
 		multiSet.print(System.out);
 		multiSet.add(5);
 		multiSet.add(3);
 		multiSet.add(7);
-		multiSet.add(3);
+		multiSet.add(8);
+		multiSet.add(7);
+		multiSet.add(5);
+		multiSet.add(5);
+		multiSet.add(7);
+		multiSet.add(2);
 		System.out.println("Print MultiSet after Adding elements");
 		multiSet.print(System.out);
-		System.out.println("Remove 3");
+		/*System.out.println("Remove 3");
 		multiSet.removeOne(3);
+		System.out.println("Remove 7");
+		multiSet.removeOne(7);
 		multiSet.print(System.out);*/
-		/***SortedLinkedMultiSet***/
-		SortedLinkedListMultiset<Integer> sortedMultiSet=new SortedLinkedListMultiset<Integer>();
-		sortedMultiSet.add(5);
-		sortedMultiSet.print(System.out);
-		sortedMultiSet.add(3);
-		sortedMultiSet.print(System.out);
-		sortedMultiSet.add(7);
-		sortedMultiSet.print(System.out);
-		sortedMultiSet.add(3);
-		System.out.println("Print MultiSet after Adding elements");
-		sortedMultiSet.print(System.out);
-		System.out.println("Remove 3");
-		sortedMultiSet.removeOne(3);
-		sortedMultiSet.print(System.out);
 	}
 
 }
