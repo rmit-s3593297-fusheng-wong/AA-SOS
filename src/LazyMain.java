52dfc2e04beaa893d413383771ae5dde2f033d47
rmit-s3593297-fusheng-wong <s3593297@student.rmit.edu.au>
2017-08-28 16:08:48 +1000

    Update LazyMain.java

--- src/LazyMain.java
+++ src/LazyMain.java
@@ -1,313 +1,233 @@
-import java.io.PrintStream;
+import java.io.*;
 import java.util.*;
 
-public class BstMultiset<T extends Comparable<T>> extends Multiset<T> implements Comparator<T>
+
+/**
+ * Framework to test the multiset implementations.
+ * 
+ * @author jkcchan
+ */
+public class MultisetTester
 {
-	Node root;
-	public BstMultiset() {
-		root=new Node(null, null, null, 0);
-	} // end of BstMultiset()
+	/** Name of class, used in error messages. */
+	protected static final String progName = "MultisetTester";
+	
+	/** Standard outstream. */
+	protected static final PrintStream outStream = System.out;
 
-	public void add(T item) {
-		Node currentNode=root;
-		//Condition for Empty Tree
-		if(currentNode==root && currentNode.getElement()==null){
-			currentNode.setElement(item);
-			currentNode.setFreq(1);
-			return;
-		}
-		addTraverse(currentNode, item);
+	/**
+	 * Print help/usage message.
+	 */
+	public static void usage(String progName) {
+		System.err.println(progName + ": <implementation> [fileName to output search results to]");
+		System.err.println("<implementation> = <linkedlist | sortedlinkedlist | bst| hash | baltree>");
+		System.exit(1);
+	} // end of usage
+
+
+	/**
+	 * Process the operation commands coming from inReader, and updates the multiset according to the operations.
+	 * 
+	 * @param inReader Input reader where the operation commands are coming from.
+	 * @param searchOutWriter Where to output the results of search.
+	 * @param multiset The multiset which the operations are executed on.
+	 * 
+	 * @throws IOException If there is an exception to do with I/O.
+	 */
+	public static void processOperations(Multiset<String> multiset, String[] inputList) 
+		throws IOException
+	{
+		String line;
+		int lineNum = 0;
+		boolean bQuit = false;
 		
-	} // end of add()
-	public void addTraverse(Node currentNode, T item){
-		//if item is smaller than current Element
-		if(currentNode.getElement().compareTo(item)==1){
-			//if Node has left child - Recursive call
-			if(currentNode.hasLeft()){
-				addTraverse(currentNode.getLeft(), item);
-			}// else Add new left child Node
-			else {
-				Node newNode=new Node(null, null, item, 1);
-				currentNode.setLeft(newNode);
-				return;
+		// continue reading in commands until we either receive the quit signal or there are no more input commands
+		while (lineNum<inputList.length) {
+			line = inputList[lineNum];
+			String[] tokens = line.split(" ");
+
+			String command = tokens[0];
+			// determine which operation to execute
+			switch (command.toUpperCase()) {
+				// add
+				case "A":
+					if (tokens.length == 2) {
+						multiset.add(tokens[1]);
+					}
+					break;
+				// search
+				case "S":
+					if (tokens.length == 2) {
+						int foundNumber = multiset.search(tokens[1]);
+						//searchOutWriter.println(tokens[1] + " " + foundNumber);
+					}
+					break;
+				// remove one instance
+				case "RO":
+					if (tokens.length == 2) {
+						multiset.removeOne(tokens[1]);
+					}
+					break;
+				// remove all instances
+				case "RA":
+					if (tokens.length == 2) {
+						multiset.removeAll(tokens[1]);
+					}
+					break;		
+				// print
+				case "P":
+					multiset.print(outStream);
+					break;
+				// quit
+				case "Q":
+					bQuit = true;
+					break;
+				default:
+					System.err.println(lineNum + ": Unknown command.");
 			}
-		}//else if Node has right child - Recursive call
-		else if(currentNode.getElement().compareTo(item)==-1){
-			if(currentNode.hasRight()){
-				addTraverse(currentNode.getRight(), item);
-			}//else Add new right child Node
-			else {
-				Node newNode=new Node(null, null, item, 1);
-				currentNode.setRight(newNode);
-				return;
-			}
-		}//else if item = element then freq + 1
-		else if(currentNode.getElement().compareTo(item)==0){
-			currentNode.setFreq(currentNode.getFreq()+1);
-			return;
+
+			lineNum++;
 		}
-	}
-	public int search(T item) {
-		Node currentNode=root;
-		//Binary Search
-		while(currentNode.hasLeft() || currentNode.hasRight()){
-			if(currentNode.getElement().compareTo(item)==1){
-				currentNode=currentNode.getLeft();
-			}
-			else if(currentNode.getElement().compareTo(item)==-1){
-				currentNode=currentNode.getRight();
-			}
-			else if(currentNode.getElement().compareTo(item)==0){
-				return currentNode.getFreq();
-			}
-		}
-		//Check Leaf Node OR Root Node
-		if(currentNode.getElement().compareTo(item)==0){
-			return currentNode.getFreq();
-		}
-		return 0;
-	} // end of add()
+
+	} // end of processOperations() 
 
 
-	public void removeOne(T item) {
-		Node currentNode=root;
-		Node previousNode=null;
-		//Check case if only Root Node exists
-		if(currentNode.equals(root) && !currentNode.hasLeft() && !currentNode.hasRight()){
-			if(currentNode.getElement().compareTo(item)==0){
-				//Check if frequency > 1 then just decrease frequency
-				if(currentNode.getFreq()>1){
-					currentNode.setFreq(currentNode.getFreq()-1);
-				}
-				else {
-					currentNode.setElement(null);
-					currentNode.setFreq(0);
-				}
-			}
-			return;
-		}//Check all internal Nodes
-		while(currentNode.hasLeft() || currentNode.hasRight()){
-			if(currentNode.getElement().compareTo(item)==1){
-				previousNode=currentNode;
-				currentNode=currentNode.getLeft();
-			}
-			else if(currentNode.getElement().compareTo(item)==-1){
-				previousNode=currentNode;
-				currentNode=currentNode.getRight();
-			}
-			else if(currentNode.getElement().compareTo(item)==0){
-				//Check if frequency > 1 then just decrease frequency
-				if(currentNode.getFreq()>1){
-					currentNode.setFreq(currentNode.getFreq()-1);
-					
-				}//Otherwise ReOrder tree
-				else removeTraverse(previousNode,currentNode,item);
-				return;
-			}
-		}
-		//Check Leaf Node OR Root Node
-		if(currentNode.getElement().compareTo(item)==0){
-			//Check if frequency > 1 then just decrease frequency
-			if(currentNode.getFreq()>1){
-				currentNode.setFreq(currentNode.getFreq()-1);
-				
-			}//Otherwise ReOrder tree
-			else removeTraverse(previousNode,currentNode,item);
-		}
-	} // end of removeOne()
-	
-	//Removes Node and ReOrders the Tree
-	public void removeTraverse(Node parentNode, Node removeNode,T item){
-		//Leaf Node
-		if(!removeNode.hasLeft() && !removeNode.hasRight()){
-			parentNode.setLeft(null);
-			parentNode.setRight(null);
-			return;
-		}
-		//If only Left Child
-		if(removeNode.hasLeft() && !removeNode.hasRight()){
-			if(removeNode!=root){
-				parentNode.setLeft(removeNode.getLeft());
-			}
-			else {
-				root=removeNode.getLeft();
-			}
-			return;
-		}//If only Right Child
-		else if(removeNode.hasRight() && !removeNode.hasLeft()){
-			if(!removeNode.equals(root)){
-				parentNode.setRight(removeNode.getRight());
-			}
-			else {
-				root=removeNode.getRight();
-			}
-			return;
-		}//If it has two children
-		else if(removeNode.hasLeft() && removeNode.hasRight()){
-			Node successorParentNode;
-			Node successorNode;
-			//Find successor -> Innermost left child of the first right
-			if(removeNode.getRight().hasLeft()){
-				//Recursively Find left most
-				successorParentNode=removeTraverseLeft(removeNode.getRight(), removeNode);
-				successorNode=successorParentNode.getLeft();
-			}
-			else {// Else successor is right child
-				successorParentNode=parentNode;
-				successorNode=removeNode.getRight();
-			}
-			/***ReOrder Tree***/
-			//Step 1 -> removeNode parent points to successor
-			if(!removeNode.equals(root)){
-				if(parentNode.getLeft()==removeNode){
-					parentNode.setLeft(successorNode);
-				}
-				else if(parentNode.getRight()==removeNode){
-					parentNode.setRight(successorNode);
-				}
-			}
-			else root=successorNode;	
-			//Step 2 -> Successor parent points to successor children
-			if(successorParentNode!=null){
-				if(successorNode.hasRight()){
-					successorParentNode.setLeft(successorNode.getRight());
-				}
-				else successorParentNode.setLeft(null);
-			}
-			//Step 3 -> Successor points to Node's children
-			successorNode.setLeft(removeNode.getLeft());
-			if(removeNode.getRight()!=successorNode){//Avoid loop to point to itself
-				successorNode.setRight(removeNode.getRight());
-			}
-			//Step 4 -> Node points to null (left & right)
-			removeNode.setLeft(null);
-			removeNode.setRight(null);
-			return;
-		}
-	}
-	
-	public Node removeTraverseLeft(Node currentNode, Node previousNode){
-		if(currentNode.hasLeft()){
-			previousNode=currentNode;
-			removeTraverseLeft(currentNode.getLeft(), previousNode);
-		}
-		return previousNode;
-	}
-	
-	public void removeAll(T item) {
-		Node currentNode=root;
-		Node previousNode=null;
-		//Check case if only Root Node exists
-		if(currentNode.equals(root) && !currentNode.hasLeft() && !currentNode.hasRight()){
-			if(currentNode.getElement().compareTo(item)==0){
-					currentNode.setElement(null);
-					currentNode.setFreq(0);
-			}
-			return;
-		}//Check all internal Nodes
-		while(currentNode.hasLeft() || currentNode.hasRight()){
-			if(currentNode.getElement().compareTo(item)==1){
-				previousNode=currentNode;
-				currentNode=currentNode.getLeft();
-			}
-			else if(currentNode.getElement().compareTo(item)==-1){
-				previousNode=currentNode;
-				currentNode=currentNode.getRight();
-			}
-			else if(currentNode.getElement().compareTo(item)==0){
-				removeTraverse(previousNode,currentNode,item);
-				return;
-			}
-		}
-		//Check Leaf Node OR Root Node
-		if(currentNode.getElement().compareTo(item)==0){
-			removeTraverse(previousNode,currentNode,item);
-		}
-	} // end of removeAll()
+	/**
+	 * Main method.  Determines which implementation to test.
+	 */
+	public static void main(String[] args) {
 
-
-	public void print(PrintStream out) {
-		Node currentNode=root;
-		printTraverse(currentNode, out);
-	} // end of print()
-	
-	public void printTraverse(Node currentNode, PrintStream out){
-		if(currentNode.hasLeft()){
-			printTraverse(currentNode.getLeft(), out);
-		}
-		out.println(currentNode.getElement() + printDelim + currentNode.getFreq());
-		if(currentNode.hasRight()){
-			printTraverse(currentNode.getRight(), out);
-		}
-	}
-	
-	//Node Class
-	public class Node{
-		private Node left;
-		private Node right;
-		private T element;
-		private int freq;
-		
-		public Node(Node left, Node right,T element, int freq){
-			this.left=left;
-			this.right=right;
-			this.element=element;
-			this.freq=freq;
-		}
+		// construct in and output streams/writers/readers, then process each operation.
+		try {
+			//generate random
+			int inputSize = 100000;
+			int dataSize = 100000;
+			String newRow = "";
 			
-		//set Frequency
-		public int setFreq(int freq){
-			return this.freq=freq;
-		}
-		
-		//get Frequency
-		public int getFreq(){
-			return this.freq;
-		}
-				
-		//get Element
-		public T getElement(){
-			return this.element;
-		}
-		
-		//set Element
-		public void setElement(T element){
-			this.element=element;
-		}
+			//this is preloaded before actions
+			String[] dataList = new String[dataSize];
+			String[] dataValues = {"EIGHT","FIVE","FOUR","NINE","ONE","SEVEN","SIX","TEN","THREE","TWO"};
 					
-		//get Left Node
-		public Node getLeft(){
-			return this.left;
-		}
-		
-		//get Right Node
-		public Node getRight(){
-			return this.right;
-		}
-		
-		//set Right Node
-		public void setRight(Node right){
-			this.right=right;
-		}
-				
-		//set Left Node
-		public void setLeft(Node left){
-			this.left=left;
-		}
+			//this is the set of actions onto preloaded data
+			String[] inputList = new String[inputSize];
+			String[] inputTypes = {"A","A","A","RO","S","S","S"};
+			String[] inputValues = {"EIGHT","FIVE","FOUR","NINE","ONE","SEVEN","SIX","TEN","THREE","TWO","RANDOM"};
+			//sorted array
+			int i=0;
+	
+			Multiset<String> multiset = new LinkedListMultiset<String>();
+			Multiset<String> multiset2 = new SortedLinkedListMultiset<String>();
+			Multiset<String> multiset3 = new LinkedListMultiset<String>();
+			Multiset<String> multiset4 = new HashMultiset<String>();
+			Multiset<String> multiset5 = new BalTreeMultiset<String>();
+			double averageLL=0;
+			double averageSLL=0;
+			double averageBST=0;
+			double averageHM=0;
+			double averageBAL=0;
+			double estimatedTime;
 			
-		//check if has Left Node
-		public boolean hasLeft(){
-			return this.getLeft()!=null?true:false;
+			long startTime;
+			long endTime;
+			for(int tries=0; tries<10; tries++) {
+				i=0;
+				//generate data
+				//generate random start
+				/*for(i=0; i<inputSize; i++) {
+					newRow = inputTypes[new Random().nextInt(inputTypes.length)]+" ";
+					newRow += inputValues[new Random().nextInt(inputValues.length)];
+					inputList[i] = newRow;
+					//System.out.println (newRow);
+				}*/
+				//generate random end
+			
+				//comment out the one that is not being used
+				//generate fixed set start
+				//generate forward sort
+					for(int out=0;out<10;out++) {
+				//generate forward sort end
+					
+				//generate backward sort
+					//for(int out=9;out>=0;out--) {
+				//generate backward sort end
+						//the -1 here is for the search at the end
+						for(int in=0;in<((inputSize/10)-1);in++) {
+							if(in <= (inputSize/10)*.6) {
+								//add first 7
+								newRow = inputTypes[0]+" ";
+								newRow += inputValues[out];
+								inputList[i] = newRow;
+								//System.out.println (newRow + " 1 " + i);
+								i++;
+							}else if(in > (inputSize/10)*.6 && in <= (inputSize/10)-1) {
+								//remove next 2
+								newRow = inputTypes[3]+" ";
+								newRow += inputValues[out];
+								inputList[i] = newRow;
+								//System.out.println (newRow + " 2 " + i);
+								i++;
+							}else {
+								//no print
+							}
+						}
+					}
+					//search
+					for(int search=0;search<10;search++) {
+						//the -1 here is for the search at the end
+						newRow = inputTypes[4]+" ";
+						newRow += inputValues[search];
+						inputList[i] = newRow;
+						//System.out.println (newRow + " 3 " + i);
+						i++;
+					}
+				//generate fixed set end
+				
+				//process the operations
+				startTime = System.nanoTime();
+				processOperations(multiset4, inputList);
+				endTime = System.nanoTime() ;
+				estimatedTime = ((double)(endTime - startTime)) / Math.pow ( 10 , 9 ) ;
+				averageHM += estimatedTime;
+				System.out.println ( "time taken = " + estimatedTime + " sec " ) ;
+				
+				startTime = System.nanoTime();
+				processOperations(multiset5, inputList);
+				endTime = System.nanoTime() ;
+				estimatedTime = ((double)(endTime - startTime)) / Math.pow ( 10 , 9 ) ;
+				averageBAL += estimatedTime;
+				System.out.println ( "time taken = " + estimatedTime + " sec " ) ;
+				
+				startTime = System.nanoTime();
+				processOperations(multiset, inputList);
+				endTime = System.nanoTime() ;
+				estimatedTime = ((double)(endTime - startTime)) / Math.pow ( 10 , 9 ) ;
+				averageLL += estimatedTime;
+				System.out.println ( "time taken = " + estimatedTime + " sec " ) ;
+				
+				startTime = System.nanoTime();
+				processOperations(multiset2, inputList);
+				endTime = System.nanoTime() ;
+				estimatedTime = ((double)(endTime - startTime)) / Math.pow ( 10 , 9 ) ;
+				averageSLL += estimatedTime;
+				System.out.println ( "time taken = " + estimatedTime + " sec " ) ;
+				
+				/*startTime = System.nanoTime();
+				processOperations(multiset3, inputList);
+				endTime = System.nanoTime() ;
+				estimatedTime = ((double)(endTime - startTime)) / Math.pow ( 10 , 9 ) ;
+				averageBST += estimatedTime;*/
+				//System.out.println ( "time taken = " + estimatedTime + " sec " ) ;
+			}
+			System.out.println ( "Average LL time taken = " + averageLL/100 + " sec " ) ;
+			System.out.println ( "Average SLL time taken = " + averageSLL/100 + " sec " ) ;
+			//System.out.println ( "Average BST time taken = " + averageBST/100 + " sec " ) ;
+			System.out.println ( "Average HM time taken = " + averageHM/100 + " sec " ) ;
+			System.out.println ( "Average BAL time taken = " + averageBAL/100 + " sec " ) ;
+			
+		} catch (IOException e) {
+			System.err.println(e.getMessage());
 		}
-							
-		//check if has Right Node
-		public boolean hasRight(){
-			return this.getRight()!=null?true:false;
-		}
-	}
 
-	@Override
-	public int compare(T o1, T o2) {
-		return o1.compareTo(o2);
-	}
-} // end of class BstMultiset
+	} // end of main()
+
+} // end of class MultisetTester
